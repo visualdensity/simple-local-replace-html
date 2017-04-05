04ae02a0a35acfbecafff3e4564358ef2ac644cc
Wee Keat Chin <weekeat@visualdensity.com>
2017-04-06 08:57:18 +1000

    Initial

---
+++ bundles/adobe/content.js
@@ -0,0 +1,5 @@
+var content = {
+    "title": "Adobe update in progress",
+    "heading": "Adobe installation in progress...",
+    "description": "Some longer description right here. Please do not shut down"
+}
